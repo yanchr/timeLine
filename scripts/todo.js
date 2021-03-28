d992c9d77825280c2ba7f0cd6b771d931679c38d
Christen, Yanick-MGB-M0365530 <yanick.christen@mgb.ch>
2021-03-28 16:03:18 +0200

    added Todo.js

---
+++ scripts/todo.js
@@ -0,0 +1,11 @@
+/**
+ * scroll in info.js
+ * place all infos in info.js
+ * add svg backgrond in script.js
+ * add insert new entry 
+ *      - right div {home Btn && new entry button}
+ *      - add entry to this json or
+ * add insert new json
+ *      -in info.js 
+ *      -right div {home Btn && new entry button}
+ */
